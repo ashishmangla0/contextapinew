aed72048d021e66a27bd948ceb96a778edd04012
ashishmangla0 <ashishmangla1991@gmail.com>
2024-04-24 12:02:03 +0530

    add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import ListData from './components/ListData';
+// import ListData from './components/ListData';
+import Home from './components/Home/Home';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Cart from './components/Cart/Cart';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-    <ListData/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path="/" exact element={<Home/>} />
+          <Route path="/cart" exact element={<Cart/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
